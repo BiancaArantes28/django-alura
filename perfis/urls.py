992e2f4f911747987354de425cdf3a4342df8224
BiancaArantes28 <biancaarantes28@gmail.com>
2017-05-04 16:38:28 -0300

    Aula de hoje

--- perfis/urls.py
+++ perfis/urls.py
@@ -6,5 +6,5 @@ urlpatterns = patterns('',
     
     url(r'^$', views.index, name='index'),
     url(r'perfis/(?P<perfil_id>\d+)$',views.exibir, name='exibir'),
-    url(r'^perfis/(?P<perfil_id>\d+)/convidar$', views.convidar, name='convidar')
+    url(r'^perfis/(?P<perfil_id>\d+)/convidar$', views.convidar , name='convidar')
 )
